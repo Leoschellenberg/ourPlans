f10b6c09e0ce18c6f6dc4533364456effb58a072
Leonardo Martins Schellenberg <leonardomartinsschellenberg@MacBook-Pro-de-MacOs.local>
2021-12-16 09:46:13 -0300

    Correção Login

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  async logout() {
+    try {
+      await this.authService.logout();
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
